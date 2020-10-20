9868f31d717b25bf3e469536c260f796adcfcf7b
Shakith <nimsara.shakith@gmail.com>
2020-10-21 00:19:55 +0530

    Added Item Spinner

---
+++ app/src/main/java/com/example/cssemobileapp/Model/Item.java
@@ -0,0 +1,57 @@
+package com.example.cssemobileapp.Model;
+
+public class Item {
+
+    String name,qty,supplier,unitPrice;
+    boolean availability;
+
+    public Item(){}
+
+    public Item(boolean availability, String name, String qty, String supplier, String unitPrice) {
+        this.availability = availability;
+        this.name = name;
+        this.qty = qty;
+        this.supplier = supplier;
+        this.unitPrice = unitPrice;
+    }
+
+    public boolean getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(boolean availability) {
+        this.availability = availability;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getQty() {
+        return qty;
+    }
+
+    public void setQty(String qty) {
+        this.qty = qty;
+    }
+
+    public String getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(String supplier) {
+        this.supplier = supplier;
+    }
+
+    public String getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(String unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+}
