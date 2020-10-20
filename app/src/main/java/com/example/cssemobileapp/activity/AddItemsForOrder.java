099dbb2e26293ecdb3164729c617504d2870f41d
Dulanji1 <mddulanjicooray@gmail.com>
2020-10-20 10:44:22 +0530

    add database

--- app/src/main/java/com/example/cssemobileapp/activity/AddItemsForOrder.java
+++ app/src/main/java/com/example/cssemobileapp/activity/AddItemsForOrder.java
@@ -1,72 +1,110 @@
-package com.example.cssemobileapp.activity;
-
-import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ScrollView;
-
-import com.example.cssemobileapp.R;
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AddItemsForOrder#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class AddItemsForOrder extends Fragment {
-
-    ImageView addnewitembtn;
-    LinearLayout list;
-
-    public AddItemsForOrder() {
-        // Required empty public constructor
-    }
-
-    public static AddItemsForOrder newInstance(String param1, String param2) {
-        AddItemsForOrder fragment = new AddItemsForOrder();
-
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_add_items_for_order, container, false);
-
-
-        return view;
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        addnewitembtn = view.findViewById(R.id.addnewitembtn);
-        list = view.findViewById(R.id.list);
-
-        addnewitembtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                View journeyView = getLayoutInflater().inflate(R.layout.additemcardview,null,false);
-                journeyView.findViewById(R.id.aaa);
-                list.addView(journeyView);
-            }
-        });
-    }
-
-
-}
+//package com.example.cssemobileapp.activity;
+//
+//import android.os.Bundle;
+//
+//import androidx.annotation.NonNull;
+//import androidx.annotation.Nullable;
+//import androidx.fragment.app.Fragment;
+//
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.Button;
+//import android.widget.ImageView;
+//import android.widget.LinearLayout;
+//import android.widget.ScrollView;
+//import android.widget.TextView;
+//
+//import com.example.cssemobileapp.R;
+//import com.google.android.gms.tasks.OnCompleteListener;
+//import com.google.android.gms.tasks.Task;
+//import com.google.firebase.firestore.FirebaseFirestore;
+//import com.google.firebase.firestore.QueryDocumentSnapshot;
+//import com.google.firebase.firestore.QuerySnapshot;
+//import com.jaredrummler.materialspinner.MaterialSpinner;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+///**
+// * A simple {@link Fragment} subclass.
+// * Use the {@link AddItemsForOrder#newInstance} factory method to
+// * create an instance of this fragment.
+// */
+//public class AddItemsForOrder extends Fragment {
+//
+//    ImageView addnewitembtn;
+//    LinearLayout list;
+//
+//    List<AddItemsForOrder> no;
+//
+//
+//    public AddItemsForOrder() {
+//        // Required empty public constructor
+//    }
+//
+//    public static AddItemsForOrder newInstance(String param1, String param2) {
+//        AddItemsForOrder fragment = new AddItemsForOrder();
+//
+//        return fragment;
+//    }
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//    }
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        View view =  inflater.inflate(R.layout.fragment_add_items_for_order, container, false);
+//
+//        return view;
+//    }
+//
+//    @Override
+//    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+//        super.onViewCreated(view, savedInstanceState);
+//        addnewitembtn = view.findViewById(R.id.addnewitembtn);
+//        list = view.findViewById(R.id.list);
+//
+//        addnewitembtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                View journeyView = getLayoutInflater().inflate(R.layout.additemcardview,null,false);
+//                journeyView.findViewById(R.id.aaa);
+//                list.addView(journeyView);
+//            }
+//        });
+////
+////        FirebaseFirestore db = FirebaseFirestore.getInstance();
+////
+////        db.collection("items")
+////                .get()
+////                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+////                    @Override
+////                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+////                        if (task.isSuccessful()) {
+////                             no = (List<AddItemsForOrder>) task.getResult().getDocuments().get("");
+////                             addDetails();
+////                        } else {
+////
+////                        }
+////                    }
+////                });
+//////    }
+////
+////    private void addDetails() {
+////
+////        View cardView = getLayoutInflater().inflate(R.layout.additemcardview,null,false);
+////
+////        MaterialSpinner spinner = (MaterialSpinner)cardView.findViewById(R.id.spinner);
+////        spinner.setItems(no);
+////
+////        //list.addView(cardView);
+////    }
+////
+////
+//}
