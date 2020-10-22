08131ba7ff5ac78d649c50b1330c89ebc509c3e0
Dulanji1 <mddulanjicooray@gmail.com>
2020-10-22 23:33:30 +0530

    add suppliers and items pages

--- app/src/main/java/com/example/cssemobileapp/activity/Item_Details.java
+++ app/src/main/java/com/example/cssemobileapp/activity/Item_Details.java
@@ -2,65 +2,102 @@ package com.example.cssemobileapp.activity;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import com.example.cssemobileapp.Model.Item;
 import com.example.cssemobileapp.R;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Item_Details#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Item_Details extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    LinearLayout list;
+
+    List<String> no1;
+    List<String> no2;
+    List<String> no3;
+    List<String> no4;
+    List<Boolean> no5;
 
     public Item_Details() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Item_Details.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static Item_Details newInstance(String param1, String param2) {
         Item_Details fragment = new Item_Details();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_item__details, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_item__details, container, false);
+
+        return view;
+    }
+
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        list = view.findViewById(R.id.list);
+
+        no1 = new ArrayList<String>();
+        no2 = new ArrayList<String>();
+        no3 = new ArrayList<String>();
+        no4 = new ArrayList<String>();
+        no5 = new ArrayList<Boolean>();
+
+
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection("items")
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        if (!queryDocumentSnapshots.isEmpty()) {
+                            List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
+                            for (DocumentSnapshot d : list) {
+                                Item i = d.toObject(Item.class);
+                                no1.add(i.getName());
+                                no2.add(i.getSupplier());
+                                no3.add(i.getUnitPrice());
+                                no4.add(i.getQty());
+                                no5.add(i.getAvailability());
+                            }
+
+
+                        }
+                    }
+
+                });
     }
 }
